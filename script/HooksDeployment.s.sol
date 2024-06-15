5cb620df132d4ba7e5566ef5a0b1975f6479aa93
OxCrispy <crispy@figarolabs.com>
2024-06-15 05:16:33 -0400

    Fix withdraw hook error

--- script/HooksDeployment.s.sol
+++ script/HooksDeployment.s.sol
@@ -106,6 +106,9 @@ contract HooksDeploymentScript is Script {
             console.logAddress(allOperators[i]);
         }
 
+        console.log("\n=== Setup PPFX, Update Withdraw Hook Address ===");
+        IPPFX(config.ppfx).updateWithdrawHook(address(withdrawHook));
+
         vm.stopBroadcast();   
     }
 }
